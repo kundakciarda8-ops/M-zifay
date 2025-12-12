13905f31c475a7b92603a9ff46a1dc9f3aeb34d7
kundakciarda8-ops <kundakciarda8@gmail.com>
2025-12-12 23:57:44 +0300

    Delete recommender_service.py

--- recommender_service.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-app = FastAPI()
-class RecRequest(BaseModel):
-    userId: str = None
-    recentTracks: list = []
-@app.post("/recommend")
-def recommend(req: RecRequest):
-    return {"recommendations": [{"id":"mock1","score":0.9},{"id":"mock2","score":0.85}]}
