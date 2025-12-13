15c1d4ec0479f4a57aa202f2c8767d4e37d4db19
kundakciarda8-ops <kundakciarda8@gmail.com>
2025-12-13 09:10:40 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
-const API_ROOT = 'http://localhost:4000'; // prod: https://api.muzifay.example.com
-export default axios.create({ baseURL: API_ROOT, timeout: 15000 });
+const API_ROOT = 'http://localhost:4000';
+export default axios.create({ 
+  baseURL: API_ROOT, 
+  timeout: 15000 
+});
